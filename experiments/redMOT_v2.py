21d9ec8b0519293afc56e358f209a910f6d59c07
Anurag Borah <anuragborah2548@gmail.com>
2024-05-29 12:57:25 +0100

    Compression added

--- experiments/redMOT_v2.py
+++ experiments/redMOT_v2.py
@@ -18,7 +18,7 @@ class redMOT_v2(EnvExperiment):
         self.setattr_argument("Cycle", NumberValue(default=1))
         self.setattr_argument("Loading_Time", NumberValue(default=550))
         self.setattr_argument("Transfer_Time", NumberValue(default=20))
-        self.setattr_argument("Holding_Time", NumberValue(default=20))
+        self.setattr_argument("Holding_Time", NumberValue(default=10))
 
     @kernel
     def run(self):
@@ -49,10 +49,10 @@ class redMOT_v2(EnvExperiment):
         delay(500*ms)
 
         for i in range(int64(self.Cycle)):
-            with parallel:
-                self.Broadband_On.pulse(10*ms)
-                self.Single_Freq.sw.off()
-            delay(4*ms)
+            # with parallel:
+            #     self.Broadband_On.pulse(10*ms)
+            #     self.Single_Freq.sw.off()
+            # delay(4*ms)
 
             # **************************** Slice 1: Loading ****************************
             # with parallel:
@@ -80,7 +80,7 @@ class redMOT_v2(EnvExperiment):
             # with parallel:
                 # Magnetic field (2.2A)
                 # with sequential:
-            voltage = 1.95
+            voltage = 3.36
             self.MOT_Coils.write_dac(0,voltage) 
             self.MOT_Coils.load()
 
@@ -105,10 +105,12 @@ class redMOT_v2(EnvExperiment):
             delay(self.Holding_Time*ms)
 
             # **************************** Slice 4: Compression ****************************
+            voltage_com = 2.55
             steps_com = 8
             t = 8/steps_com
+            change = (voltage - voltage_com)/steps_com
             for i in range(int64(steps_com)):
-                voltage_com = voltage - ((i + 1) * ((voltage - 1.44)/steps_com))
+                voltage = voltage - change
                 self.MOT_Coils.write_dac(0, voltage_com)
                 self.MOT_Coils.load()
                 delay(t*ms)
@@ -116,9 +118,9 @@ class redMOT_v2(EnvExperiment):
             #0.52=3.5A, 0.91=3.0A, 1.44=2.5A, 1.95=2.1A, 2.0=2.0A, 2.2=1.8A, 2.42=1.6A, 2.55=1.5A, 3.05=1.0A, 3.36=0.7A
 
             # **************************** Slice 5: Single Frequency ****************************
-            self.Broadband_Off.pulse(10*ms)
-            delay(8.5*ms)
-            self.Single_Freq.sw.on()
+            # self.Broadband_Off.pulse(10*ms)
+            # delay(8.5*ms)
+            # self.Single_Freq.sw.on()
 
             # **************************** Slice 6: Shutter delay ****************************
             with parallel:
@@ -129,13 +131,7 @@ class redMOT_v2(EnvExperiment):
             # **************************** Slice 5: Detection ****************************
             with parallel:
                 self.BMOT_AOM.set(frequency=90*MHz, amplitude=0.09)
-                self.Camera.pulse(5*ms)
-
-            delay(50*ms)
-
-            # **************************** Slice 6 ****************************
-            self.ZeemanSlower.set_att(0.0)
-            self.ZeemanSlower.set(frequency=180 * MHz, amplitude=0.35)
+                self.Camera.pulse(10*ms)
             
             # **************************** Slice 7 ****************************
             delay(1000*ms)
