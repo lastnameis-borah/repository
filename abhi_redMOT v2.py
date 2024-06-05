62a9bbc8e18c782e78bd8ce1e4134603cb96afd7
Anurag Borah <anuragborah2548@gmail.com>
2024-06-05 13:58:37 +0100

    Changed holding time

--- abhi_redMOT v2.py
+++ abhi_redMOT v2.py
@@ -1,9 +1,8 @@
 from artiq.experiment import *
 from artiq.coredevice.ttl import TTLOut
 from numpy import int64
-# from repository.regression1 import Gate_Voltage
 
-class Abhi_redMOT_Transfer_time(EnvExperiment):
+class abhi_redMOT_v3(EnvExperiment):
     def build(self):
         self.setattr_device("core")
         self.Camera:TTLOut=self.get_device("ttl10")
@@ -19,7 +18,7 @@ class Abhi_redMOT_Transfer_time(EnvExperiment):
         self.setattr_argument("Cycle", NumberValue(default=1))
         self.setattr_argument("Loading_Time", NumberValue(default=550))
         self.setattr_argument("Transfer_Time", NumberValue(default=20))
-        self.setattr_argument("Holding_Time", NumberValue(default=20))
+        self.setattr_argument("Holding_Time", NumberValue(default=10))
 
     @kernel
     def run(self):
@@ -47,95 +46,107 @@ class Abhi_redMOT_Transfer_time(EnvExperiment):
         self.Single_Freq.set_att(0.0)
         self.Single_Freq.set(frequency= 80 * MHz, amplitude=1.0)
 
-        delay(1000*ms)
-        self.core.break_realtime()
+        delay(500*ms)
+        delay_mu(int64(self.core.ref_multiplier))
 
         for i in range(int64(self.Cycle)):
-            # Slice 1
-            # with parallel:
-                # BMOT
+            # **************************** Slice 1: Loading ****************************
+            # BMOT
             self.BMOT_AOM.set(frequency=90*MHz, amplitude=0.09)
 
-                # Zeeman Slower
+            # Zeeman Slower
             self.ZeemanSlower.set(frequency=180 * MHz, amplitude=0.35)
 
             with parallel:
-                # Magnetic field (3.0v)
                 with sequential:
                     voltage = 0.52
                     self.MOT_Coils.write_dac(0, voltage)
                     self.MOT_Coils.load()
-                
-                # Start the modulation of red
-                self.RMOT_TTL.on()
                 self.BMOT_TTL.on()
+                self.RMOT_TTL.on()
+                self.Broadband_On.pulse(10*ms)
+                self.Single_Freq.sw.off()
+            
 
             # Slice 1 duration
             delay(self.Loading_Time*ms)
 
-            # Slice 2: Transfer
+            # **************************** Slice 2: Transfer ****************************
 
             # with parallel:
                 # Magnetic field (2.2A)
                 # with sequential:
-            # print(f"{Gate_Voltage(2.1)[0][0]:.2f}")
-            # voltage = 0.52
-            # self.MOT_Coils.write_dac(0, voltage)
-            # #1.23=2.7A, 1.55=2.4A, 1.95=2.1A, 2.0=2.0A, 2.26=1.8A, 2.42=1.6A, 2.55=1.5A, 2.85=1.2A, 3.05=1.0A, 3.18=0.9A, 3.36=0.7A, 3.5=0.6A
-            # self.MOT_Coils.load()
+            voltage = 2.55
+            self.MOT_Coils.write_dac(0,voltage) 
+            self.MOT_Coils.load()
+            #0.52=3.5A, 0.91=3.0A, 1.44=2.5A, 1.95=2.1A, 2.0=2.0A, 2.2=1.8A, 2.42=1.6A, 2.55=1.5A, 3.05=1.0A, 3.36=0.7A
 
                 # Zeeman Slower
                 # with sequential:
             # self.ZeemanSlower.set_att(31.9)
-            self.ZeemanSlower.set(frequency=180*MHz, amplitude=0.0)
+            self.ZeemanSlower.set(frequency=180 * MHz, amplitude=0.0)
 
             # BMOT
             with parallel:
                 with sequential:
                     steps = self.Transfer_Time
                     t = self.Transfer_Time/steps
-                    for i in range(int64(steps + 1.0)):
+                    for i in range(int64(steps)):
                         amp_steps = 0.09/steps
-                        amp = 0.09 - (i * amp_steps)
+                        amp = 0.09 - ((i+1) * amp_steps)
                         self.BMOT_AOM.set(frequency=90*MHz, amplitude=amp)
                         delay(t*ms)
 
                 with sequential:
-                    steps = self.Transfer_Time
-                    t = self.Transfer_Time/steps
                     change = ((3.36 - voltage)/steps)
                     for i in range(int64(steps)):
                         voltage = voltage + change
                         self.MOT_Coils.write_dac(0, voltage)
                         self.MOT_Coils.load()
                         delay(t*ms)
-                        # print(voltage)
 
             self.BMOT_TTL.off()
 
-            # Slice 3
+            # **************************** Slice 3: Holding ****************************
             delay(self.Holding_Time*ms)
 
-            # Slice 4: Compentation for shutter delay
-            self.RMOT_TTL.off()
-            self.BMOT_TTL.on()
+            # **************************** Slice 4: Compression ****************************
+            # voltage_com = 2.55
+            # steps_com = 8
+            # t = 8/steps_com
+            # change = (voltage - voltage_com)/steps_com
+            # for i in range(int64(steps_com)):
+            #     voltage = voltage - change
+            #     self.MOT_Coils.write_dac(0, voltage_com)
+            #     self.MOT_Coils.load()
+            #     delay(t*ms)
+
+            #0.52=3.5A, 0.91=3.0A, 1.44=2.5A, 1.95=2.1A, 2.0=2.0A, 2.2=1.8A, 2.42=1.6A, 2.55=1.5A, 3.05=1.0A, 3.36=0.7A
+
+            # **************************** Slice 5: Single Frequency ****************************
+            # with parallel:
+            #     self.Broadband_Off.pulse(10*ms)
+            #     self.Single_Freq.sw.on()
+
+            # delay(10*ms)
+
+            # **************************** Slice 6: Shutter delay ****************************
+            with parallel:
+                self.RMOT_TTL.off()
+                self.BMOT_TTL.on()
             delay(3*ms)
 
-            # Slice 5: Detection
+            # **************************** Slice 5: Detection ****************************
             with parallel:
-                with sequential:
-                    self.MOT_Coils.write_dac(0, 4.15)
-                    self.MOT_Coils.load()   
                 self.BMOT_AOM.set(frequency=90*MHz, amplitude=0.09)
-                self.Camera.pulse(5*ms)
-
-            delay(50*ms)
-
-            # Slice 6
-            self.ZeemanSlower.set_att(0.0)
-            self.ZeemanSlower.set(frequency=180 * MHz, amplitude=0.35)
+                self.Camera.pulse(10*ms)
             
-            # Slice 6: Headroom for 2nd cycle
+            # **************************** Slice 7 ****************************
+            
+            # with parallel:
+
+                # self.Broadband_On.pulse(10*ms)
+                # self.ZeemanSlower.set(frequency=180 * MHz, amplitude=0.35)
             delay(1000*ms)
 
         print("RedMOT exp complete!!")
