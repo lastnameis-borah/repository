50456fb159fbd641b4bb4575086ec8e505229cb6
Anurag Borah <anuragborah2548@gmail.com>
2024-07-04 11:16:56 +0100

    --

--- a.py
+++ a.py
@@ -1,7 +1,11 @@
-voltage = 0.52
-steps = 20
-t = 20/steps
-change = (1.95 - voltage)/steps
-for i in range(steps):
-    voltage = voltage + change
-    print(voltage)
+voltage = 3.45
+voltage_com = 2.75
+steps_com = 8
+t_com = 8/steps_com
+change = (voltage - voltage_com)/steps_com
+
+for i in range(steps_com):
+    voltage = voltage - change
+    self.MOT_Coils.write_dac(0, voltage_com)
+    self.MOT_Coils.load()
+    delay(t_com*ms)
