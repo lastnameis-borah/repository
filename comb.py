6f743f7f0c5c8b9933c6047499bd6462ec9dd59b
Anurag Borah <anuragborah2548@gmail.com>
2024-08-22 13:18:43 +0100

    comb lock

--- comb.py
+++ comb.py
@@ -2,21 +2,39 @@ from artiq.experiment import *
 from artiq.coredevice.ttl import TTLOut
 from numpy import int64
 
-class comb(EnvExperiment):
+class test_rf(EnvExperiment):
     def build(self):
         self.setattr_device("core")
-        self.rf=self.get_device("urukul0_ch0")
+        self.Single_Freq=self.get_device("urukul1_ch0")
+        self.Probe=self.get_device("urukul0_ch0")
 
     @kernel
     def run(self):
         self.core.reset()
         self.core.break_realtime()
-        self.rf.cpld.init()
-        self.rf.init()
-        # self.core.break_realtime()
+        self.Single_Freq.cpld.init()
+        self.Single_Freq.init()
+        self.Probe.cpld.init()
+        self.Probe.init()
 
-        self.rf.sw.on()
-        self.rf.set_att(16.0)
-        self.rf.set(frequency=80*MHz)
+        self.Single_Freq.sw.on()
+        self.Single_Freq.set_att(0.0)
+        self.Probe.set_att(10.0)
+        self.Probe.set(frequency=80*MHz)
+
+
+        for i in range(int64(20)):
+            self.Single_Freq.set(frequency=80*MHz, amplitude=0.5)
+            self.Probe.sw.on()
+            delay(500*ms)
+            self.Single_Freq.set(frequency=80*MHz, amplitude=0.0)
+            self.Probe.sw.off()
+            delay(500*ms)
+
+        # self.Probe.sw.on()
+
+        # delay(500*ms)
+
+        # self.Probe.sw.off()
 
         print("RF set")
