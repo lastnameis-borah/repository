cc3098b3b7279ea7964b29881a5b51e6efe649d1
Anurag Borah <anuragborah2548@gmail.com>
2025-03-06 17:17:14 +0000

    reversal

--- Clock.py
+++ Clock.py
@@ -285,7 +285,4 @@ class clock(EnvExperiment):
             self.Broadband_On.pulse(10*ms)
             # self.BMOT_TTL.on()
             delay(2000*ms)
-
-            
-            #asdasd
             #asdsad
