83888c3f53b43ecf4daefba61a9f2788e554be22
Anurag Borah <anuragborah2548@gmail.com>
2025-03-27 12:45:13 +0000

    scan range chsnge

--- experiments/Clock_Transition.py
+++ experiments/Clock_Transition.py
@@ -87,8 +87,8 @@ class clock_transition_lookup(EnvExperiment):
         # start_freq = 85.477
         # end_freq = 85.473
 
-        start_freq = 85.55
-        end_freq = 85.45
+        start_freq = 85.5
+        end_freq = 85.494
         res = (end_freq - start_freq)/int64(self.Cycle)
 
         for j in range(int64(self.Cycle) + 1):
