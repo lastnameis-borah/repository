93bc7a3dae1427fcb3a185f072e66a9ac38348ae
Anurag Borah <anuragborah2548@gmail.com>
2025-05-28 13:01:21 +0100

    voltage update

--- experiments/Clock_Transition.py
+++ experiments/Clock_Transition.py
@@ -185,8 +185,8 @@ class clock_transition_lookup(EnvExperiment):
             self.Single_Freq.sw.off()
 
             # **************************** Slice 5: State Preparation *****************************
-            self.MOT_Coil_1.write_dac(0, 7.07)# 5.576/2.23 = 1.85; 7.01/0.56 = 3.5;
-            self.MOT_Coil_2.write_dac(1, 0.54)
+            self.MOT_Coil_1.write_dac(0, 5.58)# 5.58/2.28 = 1.85; 7.07/0.54 = 3.5;
+            self.MOT_Coil_2.write_dac(1, 2.28)
             with parallel:
                 self.MOT_Coil_1.load()
                 self.MOT_Coil_2.load()
